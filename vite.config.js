a193e8e2c2084a115cd29414fe3e12fe6406b2ef
Sainag <saigadangi@gmail.com>
2025-05-08 20:12:52 +0530

    Fix deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/QRify/', // This makes all asset URLs relative to the current directory
+
+  base: '/qrify/', // GitHub repository name
   build: {
     rollupOptions: {
       output: {
